de50933ab6cb3a58b64047b3dd481e16b5dc49ff
Scott Hatcher <scott@scotthatcher.com>
2014-09-25 16:04:59 -0700

    Allow a custom container class to be specified and an input width to be provided as a function.  Also improved ngdocs.

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -5,21 +5,21 @@ NG_DOCS={
   "pages": [
     {
       "section": "api",
-      "id": "angular-point.directive:apInput",
-      "shortName": "apInput",
-      "type": "directive",
-      "moduleName": "angular-point",
-      "shortDescription": "Please update the description and restriction.",
-      "keywords": "_please angular-point api description directive restriction update"
-    },
-    {
-      "section": "api",
       "id": "angular-point.directive:apInputGroup",
       "shortName": "apInputGroup",
       "type": "directive",
       "moduleName": "angular-point",
-      "shortDescription": "Please update the description and restriction.",
-      "keywords": "_please angular-point api description directive restriction update"
+      "shortDescription": "Creates individual form controls for each of the field names provided.",
+      "keywords": "allow angular-point api appear bind choices column control controls creates custom definition description directive disable dropdown element entity field fieldname form individual input item label layout list logic model names ng-disabled objecttype optional optionally override pass property provided set sharepoint stored text types valid validation width"
+    },
+    {
+      "section": "api",
+      "id": "angular-point.directive:apInputRow",
+      "shortName": "apInputRow",
+      "type": "directive",
+      "moduleName": "angular-point",
+      "shortDescription": "Creates individual form controls for each of the field names provided.",
+      "keywords": "angular-point api class control controls create creates directive disable element entity field fields form individual item list names ng-disabled optional override pass properties provided row set sharepoint"
     }
   ],
   "apis": {
