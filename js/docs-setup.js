2073c2eb30c865ad0095608feca017bfe9a07d00
Scott Hatcher <scott@scotthatcher.com>
2014-09-27 12:32:03 -0700

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,35 @@
+NG_DOCS={
+  "sections": {
+    "api": "Form Control Directive Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angular-point.directive:apInputRow",
+      "shortName": "apInputRow",
+      "type": "directive",
+      "moduleName": "angular-point",
+      "shortDescription": "Creates individual form controls for each of the field names provided.",
+      "keywords": "angular-point api class control controls create creates directive disable element entity field fields form individual item list names ng-disabled optional override pass properties provided row set sharepoint"
+    },
+    {
+      "section": "api",
+      "id": "angularPoint.apInputGroup",
+      "shortName": "angularPoint.apInputGroup",
+      "type": "directive",
+      "moduleName": "angularPoint",
+      "shortDescription": "Creates the appropriate input type for an angular-point list item field.  Binds the field to the entity, passes",
+      "keywords": "$value allows angular-point angularpoint api apinputgroup appear appropriate attempt attribute attributes automatically bind binds bootstrap built cache cached checks choice choices class col-sm-12 col-sm-3 column consistency control convert create creates current currentvalue custom defaults defined definition description determine directive disable display displayname don dropdown dynamically element empty entity extend fetch field fielddefinition fieldname find form function functionality group guid html individual input inputs invalid isn item label layout list logic lookup lookupoptions lowercase manages manually mark max maxlength message min minlength model named ng-disabled node normal object optional optionally options override overrides parameters params pass passed passes placehoder property provided receives referencing request required returns rows server set sharepoint simply size standard stored text title true type types typically ui-date val valid validation validity values width wiring"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api/angularPoint.apInputGroup",
+  "scripts": [
+    "angular.js",
+    "angular-animate.min.js"
+  ]
+};
